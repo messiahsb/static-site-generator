deb8e2998c2f66792b24fa0a712e94d8d27cc8e8
messiahsb <bonet.messiah@gmail.com>
2026-05-14 15:02:04 -0400

    committing

--- src/block_parser.py
+++ src/block_parser.py
@@ -1,4 +1,15 @@
 
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     return_blocks = []
@@ -7,3 +18,49 @@ def markdown_to_blocks(markdown):
             return_blocks.append(block.strip())
 
     return return_blocks
+
+def  block_to_block_type(block):
+
+
+    headings = ('# ', '## ', '### ' '#### ', '##### ', '###### ')
+    if block.startswith(headings):
+        return BlockType.HEADING
+
+    # checks for a code block
+    if block[0] == "`":
+        lines = block.split('\n')
+        if lines[0].strip()  == "```" and lines[-1].strip()  == "```" :
+            return BlockType.CODE
+
+    # checks for a quote block
+    if block[0] == ">":
+        lines = block.split('\n')
+        for line in lines:
+            if line[0] != ">":
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    # checks for an ordered block
+    if block[0] == "1":
+        lines = block.split('\n')
+        for idx, line in enumerate(lines):
+            if len(line) < 3 or line[0:3] != f"{idx+1}. ":
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+
+    # checks for an unordered block
+    if block[0] == "-":
+        lines = block.split('\n')
+        for idx, line in enumerate(lines):
+            if len(line) < 2 or line[0:2] != "- ":
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+
+    return BlockType.PARAGRAPH
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        
--- src/test_blockparser.py
+++ src/test_blockparser.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_parser import markdown_to_blocks
+from block_parser import markdown_to_blocks, BlockType, block_to_block_type
 
 
 
@@ -60,6 +60,98 @@ And will this line be included
                 ],
         )   
 
+        def test_code_block(self):
+            block = "```\nThis is a code block test by two lines will this work\nAnd will this line be included\n```"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.CODE
+        )   
+        def test_bad_code_block(self):
+            block = "```\nThis is a code block test by two lines will this work\nAnd will this line be included\n``"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.PARAGRAPH
+        )   
+
+        def test_quote_block(self):
+            block = "> This is a quote block test by two lines will this work\n> And will this line be included"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.QUOTE
+        )   
+        def test_bad_quote_block(self):
+            block = "> This is a quote block test by two lines will this work\nAnd will this line be included"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.PARAGRAPH
+        )   
+
+        def test_ordered_block(self):
+            block = "1. This is a quote block test by two lines will this work\n2. And will this line be included"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.ORDERED_LIST
+        )   
+        def test_bad_ordered_block(self):
+            block = "1. This is a quote block test by two lines will this work\n2.And will this line be included"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.PARAGRAPH
+        )   
+
+        def test_bad_ordered_block_p2(self):
+            block = "1. This is a quote block test by two lines will this work\n2. And will this line be included\n3."
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.PARAGRAPH
+        )   
+
+        def test_unordered_block(self):
+            block = "- This is a quote block test by two lines will this work\n- And will this line be included"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.UNORDERED_LIST
+        )   
+
+        def test_bad_unordered_block(self):
+            block = "- This is a quote block test by two lines will this work\n-And will this line be included"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.PARAGRAPH
+        )   
+
+        def test_bad_unordered_block_p2(self):
+            block = "- This is a quote block test by two lines will this work\n- And will this line be included\n-"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.PARAGRAPH
+        )   
+
+        def test_heading_block(self):
+            block = "# This is a quote block test by two lines will this work\n## And will this line be included"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.HEADING
+        )   
+
+        def test_bad_heading_block(self):
+            block = "#This is a quote block test by two lines will this work\n#And will this line be included\n-"
+            block_type =  block_to_block_type(block)
+            self.assertEqual(
+                 block_type,
+                 BlockType.PARAGRAPH
+        )   
 
 
 if __name__ == "__main__":
